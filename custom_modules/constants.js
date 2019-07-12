40189b4531049e37f743492ed708befc3db9bd6f
Jeffrey Stylos <jsstylos@us.ibm.com>
2019-07-12 12:34:10 -0400

    Initial attempts at html support

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -3,9 +3,9 @@ module.exports = {
     login: 'https://www.okcupid.com/login',
     rate: 'http://www.okcupid.com/quickmatch',
     visit_user: 'http://www.okcupid.com/profile/{username}',
-    user_profile: 'http://www.okcupid.com/profile/{username}?okc_api=1',
-    user_questions: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&low={low}',
-    get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
+    user_profile: 'http://www.okcupid.com/profile/{username}',
+    user_questions: 'http://www.okcupid.com/profile/{username}/questions?low={low}',
+    get_visitors: 'http://www.okcupid.com/visitors',
     quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
 
     // OAuth API
